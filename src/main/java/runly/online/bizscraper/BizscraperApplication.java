5c863baece685a3a322c077f03a669db2b1f70fa
svinstvo-og <maxim12.05.2006koval@gmail.com>
2025-06-15 06:54:24 +0200

    First commit

---
+++ src/main/java/runly/online/bizscraper/BizscraperApplication.java
@@ -0,0 +1,13 @@
+package runly.online.bizscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizscraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizscraperApplication.class, args);
+	}
+
+}
